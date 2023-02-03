60b5bb4afc63f2d16f26111a7f770062121ce487
Yohann <yohann@mariloo.fr>
2023-02-03 23:12:13 +0100

    Created main layout

--- src/jsMain/kotlin/MainView.kt
+++ src/jsMain/kotlin/MainView.kt
@@ -1,6 +1,51 @@
+import browser.document
+import kotlinx.html.id
 import react.Props
+import react.dom.*
 import react.fc
 
 val MainView = fc<Props> {
 
+    div {
+        attrs {
+            id = "container"
+        }
+
+        div {
+            attrs {
+                id = "resources"
+            }
+        }
+
+        div {
+            attrs {
+                id = "bars"
+            }
+        }
+
+        div {
+            attrs {
+                id = "gameview"
+            }
+        }
+
+        div {
+            attrs {
+                id = "items"
+            }
+        }
+    }
+
+    div {
+        attrs {
+            id = "sidebar"
+        }
+
+    }
+
+    script {
+        var js = document.createElement("script")
+        js.setAttribute("src", "mainView.js")
+        document.body.appendChild(js)
+    }
 }
