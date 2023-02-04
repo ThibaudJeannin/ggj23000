a4b17771245aa9e4813facdcaa6cc715f815c28d
thibaudjeannin <thibaud.jeannin@gmail.com>
2023-02-04 14:25:30 +0100

    fix db url

--- src/jvmMain/kotlin/Server.kt
+++ src/jvmMain/kotlin/Server.kt
@@ -34,7 +34,7 @@ fun Application.module() {
     val dbHost: String = env.getOrDefault("POSTGRESQL_ADDON_HOST", "localhost")
     val dbPort: String = env.getOrDefault("POSTGRESQL_ADDON_PORT", "5432")
     val dbBase: String = env.getOrDefault("POSTGRESQL_ADDON_DB", "ggj23_db")
-    val dbUrl = "jdbc:postgresql://$dbHost:$dbPort/"
+    val dbUrl = "jdbc:postgresql://$dbHost:$dbPort"
     val dbUser: String = env.getOrDefault("POSTGRESQL_ADDON_USER", "postgres")
     val dbPassword: String = env.getOrDefault("POSTGRESQL_ADDON_PASSWORD", "password")
 
