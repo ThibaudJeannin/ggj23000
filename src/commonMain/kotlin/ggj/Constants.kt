d3ec9349ff00e2b90bc120667841584b4ce443bc
Baptiste Minervini <baptiste.minervini@livdeo.fr>
2023-02-04 11:50:27 +0100

    moved constants values inside a separated file

---
+++ src/commonMain/kotlin/ggj/Constants.kt
@@ -0,0 +1,11 @@
+package ggj
+
+const val FRUIT_BIO_EVO_MODIFYER = 0.001f
+
+const val WOOD_AIR_EVO_MODIFYER = 0.0001f
+const val WOOD_BIO_MODIFYER = 0.02f
+const val WOOD_BIO_EVO_MODIFYER = 0.001f
+
+const val WOOD_PRODUCTION = 5.0f
+const val FRUITS_PRODUCTION = 5.0f
+const val IRON_PRODUCTION = 5.0f
