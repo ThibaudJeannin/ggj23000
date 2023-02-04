320f768a1d79264c1d82f89ab49471065487a897
BrunoFL <follet-bruno@hotmail.fr>
2023-02-04 14:26:51 +0100

    fix

--- src/commonMain/resources/mainView.js
+++ src/commonMain/resources/mainView.js
@@ -1,4 +1,30 @@
 document.getElementById('strip_1').addEventListener('click', () => {
-   document.getElementById('sidebar').classList.toggle('opened');
-   document.getElementById('strip_1').classList.toggle('opened');
-});
+  document.getElementById('sidebar').classList.toggle('opened')
+  document.getElementById('strip_1').classList.toggle('opened')
+})
+
+
+fetchParcelle = () => {
+  fetch('api/parcelle')
+    .then(r => r.json())
+    .then(data => {
+      console.log(data)
+    })
+}
+
+fetchMe = () => {
+  fetch('api/users/me')
+    .then(r => r.json())
+    .then(data => {
+      console.log(data)
+    })
+}
+
+fetchMe()
+fetchParcelle()
+
+
+
+
+
+
