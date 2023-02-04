cbeeb7386d51b6b6dbb49ca3cd76803d0a40e8d9
thibaudjeannin <thibaud.jeannin@gmail.com>
2023-02-04 18:43:17 +0100

    Handle redirects in ktor client

--- src/jvmMain/kotlin/ggj/RunTest.kt
+++
@@ -1,7 +0,0 @@
-package ggj
-
-
-//    https://github.com/JetBrains/Exposed
-//    https://www.baeldung.com/kotlin/exposed-persistence
-
-//https://www.optimadata.nl/blogs/1/n8dyr5-how-to-run-postgres-on-docker-part-1
