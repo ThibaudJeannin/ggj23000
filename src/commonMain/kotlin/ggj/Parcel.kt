706a7b068f347ec94127a321be4e1e20983bc305
Baptiste Minervini <baptiste.minervini@livdeo.fr>
2023-02-04 11:10:15 +0100

    fixed seed loot rate (added unit test too), added fruit harvest function on parcel model + unit tests linked

--- src/commonMain/kotlin/ggj/Parcel.kt
+++ src/commonMain/kotlin/ggj/Parcel.kt
@@ -1,5 +1,6 @@
 package ggj
 
+import kotlin.math.abs
 import kotlin.random.Random
 
 class Parcel() {
@@ -9,6 +10,10 @@ class Parcel() {
     val naturalResources = NaturalResources()
     val items = mutableListOf<Item>()
 
+    private var harvestWoodValue = 10.0f
+    private var harvestFruitValue = 2.0f
+    private var seedLootRate = 5;
+
     fun produceResources() {
         resourceStorage.addProduction(Production(5.0f, 5.0f, 5.0f))
     }
@@ -17,12 +22,24 @@ class Parcel() {
         if (naturalResources.trees - 1 < 0) {
             throw InsufficientNaturalResourcesException("cannot harvest natural resource : insufficient wood")
         }
+
         naturalResources.trees--
-        resourceStorage.wood.addToStorage(10.0f)
+        resourceStorage.wood.addToStorage(harvestWoodValue)
         indicators.bio.value -= 0.02f
         indicators.bio.evolution -= 0.001f
         indicators.air.evolution -= 0.0001f
-        if (Random.nextInt() % 100 < 15) {
+
+        if (abs(Random.nextInt()) % 100 < seedLootRate) {
+            this.items.add(Seed())
+        }
+    }
+
+    fun harvestFruits() {
+
+        resourceStorage.fruits.addToStorage(harvestFruitValue)
+        indicators.bio.evolution -= 0.001f
+
+        if (abs(Random.nextInt()) % 100 < (seedLootRate * 2)) {
             this.items.add(Seed())
         }
     }
